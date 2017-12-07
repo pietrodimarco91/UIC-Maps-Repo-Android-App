105d093318b22711f7a5093580c49a1e6dd625f4
pietro di marco <pietrodimarco91@gmail.com>
2017-12-06 23:56:01 -0600

    searchbar

--- app/src/main/java/com/example/pietrodimarco/hci/LoadJson.java
+++ app/src/main/java/com/example/pietrodimarco/hci/LoadJson.java
@@ -49,5 +49,20 @@ public class LoadJson extends Fragment {
     }
 
 
+    public String loadJSONFromAssetAll(Context appContext) {
+        String json = null;
+        try {
+            InputStream is = appContext.getAssets().open("map.geojson");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
 
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
 }
---
+++ app/src/main/java/com/example/pietrodimarco/hci/data/ResSearch.java
@@ -0,0 +1,21 @@
+package com.example.pietrodimarco.hci.data;
+
+import com.mapbox.mapboxsdk.geometry.LatLng;
+
+/**
+ * Created by pietrodimarco on 12/6/17.
+ */
+
+public class ResSearch {
+    
+    String room;
+    LatLng latLng;
+    public ResSearch(String room, LatLng latLng) {
+        this.room=room;
+        this.latLng=latLng;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+}
